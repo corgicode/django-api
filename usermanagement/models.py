3b133ba01e949baf06b34383234e62c2e1c2b0cd
David Silva <omard.skp@gmail.com>
2018-10-02 01:45:26 -0400

    Reran makemigrations and ensured that createsuperuser works

--- usermanagement/models.py
+++ usermanagement/models.py
@@ -9,6 +9,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.conf import settings
 from safedelete.models import SafeDeleteModel, SOFT_DELETE
 from safedelete.managers import SafeDeleteManager
+from model_utils import FieldTracker
 import jwt
 
 class JWT:
@@ -22,14 +23,16 @@ class JWT:
         return jwt.decode(encoded, settings.SECRET_KEY)
 
 class UserManager(SafeDeleteManager, BaseUserManager):
+    use_in_migrations = True
+
     def create_user(self, email, password=None, unusable_password=False, **kwargs):
         user = self.model(email=email, password=password, **kwargs)
 
         user.save(using=self._db, unusable_password=unusable_password)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password,)
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, password=password, username=username)
         user.is_admin = True
         user.save(using=self._db)
         return user
@@ -38,26 +41,29 @@ class UserManager(SafeDeleteManager, BaseUserManager):
         case_insensitive_username_field = '{}__iexact'.format(self.model.USERNAME_FIELD)
         return self.get(**{case_insensitive_username_field: username})
 
-class User(SafeDeleteModel, AbstractBaseUser):
+class User(AbstractBaseUser, SafeDeleteModel):
 
     _safedelete_policy = SOFT_DELETE
 
-    USERNAME_FIELD = 'username'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    password = models.CharField(max_length=128,
-                                verbose_name='password',
-                                validators=[password_validation.validate_password])
+    password = models.CharField(
+        max_length=128,
+        verbose_name='password',
+        validators=[password_validation.validate_password]
+    )
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
         unique=True,
     )
+    username = models.CharField(max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
-    deleted = models.BooleanField(default=False)
     flagged = models.BooleanField(default=False)
     private = models.BooleanField(default=False)
     name = models.CharField(max_length=50, null=True, blank=True)
@@ -65,7 +71,15 @@ class User(SafeDeleteModel, AbstractBaseUser):
     heroImage = models.TextField(blank=True, null=True)
     github_id = models.TextField(max_length=255, blank=True)
 
+    tracker = FieldTracker()
+
     objects = UserManager()
+
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
 
     def __str__(self):
         return self.username
@@ -93,6 +107,9 @@ class User(SafeDeleteModel, AbstractBaseUser):
                            'token': default_token_generator.make_token(self)})
 
     def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
         return self.name
 
     def has_perm(self, perm, obj=None):
