ab370fb615587f9887de084a8bd43ee96c0b1f4d
David Silva <omard.skp@gmail.com>
2018-10-07 13:35:32 -0400

    Created failing test to read tag and its challenge

--- challenges/tests/test_requests/test_challenges.py
+++ challenges/tests/test_requests/test_challenges.py
@@ -54,7 +54,7 @@ class ChallengeApiTests(APITestCase):
 
     def test_challenge_create_with_tags(self):
         """
-        Ensure we get the correct challenge
+        Ensure we get the correct tags information with a challenge
         """
         tag1 = Tag.objects.create(name='Javascript')
 
@@ -75,7 +75,7 @@ class ChallengeApiTests(APITestCase):
 
     def test_challenge_create_with_source(self):
         """
-        Ensure we get the correct challenge
+        Ensure we get the correct sources information with the challenge
         """
         challenge = self.get_sample_challenge()
         source = Source.objects.create(
@@ -93,3 +93,25 @@ class ChallengeApiTests(APITestCase):
         included = response_data['included']
 
         [item for item in included if item.get('type') == 'sources'][0]['attributes']['name'].should.equal(source.name)
+
+    def get_challenges_with_tag_name(self):
+        """
+        Ensure we get the correct challenge
+        """
+        tag = Tag.objects.create(name='testTag')
+
+        challenge1 = self.get_sample_challenge(tags=[ tag ])
+        challenge2 = self.get_sample_challenge(tags=[ tag ])
+
+        response = self.client.get(f'/services/api/tags?pk={ tag.name }',
+                                   content_type=self.content_type)
+
+        response.status_code.should.equal(200)
+
+        response_data = json.loads(response.content)
+        attributes = response_data['data'][0]['attributes']
+        included = response_data['included']
+
+        attributes['name'].should.equal(tag.name)
+
+        len([item for item in included if item.get('type') == 'challenge']).should.equal(2)
