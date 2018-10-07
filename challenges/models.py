2a9113aad6f8d68aed6404c8f489e3fe8551b8f3
David Silva <omard.skp@gmail.com>
2018-10-06 21:19:31 -0400

    created models and migrations for challenges

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    def __str__(self):
+        return self.name
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=250,)
+
+class Challenge(models.Model):
+
+    CHALLENGE_TYPES = (
+        ('feature', 'Feature'),
+        ('bug', 'Bug'),
+        ('improvement', 'Improvement'),
+        ('task', 'Task'),
+        ('subtask', 'Sub Task'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_visible = models.BooleanField(default=True)
+    title = models.CharField(max_length=250,)
+    short_title = models.CharField(max_length=250,)
+    owner = models.CharField(max_length=250,)
+    difficulty = models.CharField(max_length=250,)
+    challenge_type = models.CharField(max_length=30, choices=CHALLENGE_TYPES, null=True, blank=True)
+    priority = models.CharField(max_length=250)
+    description = models.TextField()
+    short_description = models.TextField()
+    extra_points = models.TextField()
+    technical_notes = models.TextField()
+    procedure = models.TextField()
+    code_tips = models.TextField()
+
+class Attachment(models.Model):
+    def __str__(self):
+        return self.name
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=250,)
+    url = models.TextField()
+    active = models.BooleanField(default=True)
+    challenge = models.ForeignKey(Challenge, on_delete=models.DO_NOTHING)
+
+class Source(models.Model):
+    def __str__(self):
+        return self.name
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=250,)
+    url = models.TextField()
+    active = models.BooleanField(default=True)
+    challenge = models.ForeignKey(Challenge, on_delete=models.DO_NOTHING)
