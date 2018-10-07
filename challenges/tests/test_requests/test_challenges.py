28a8e186fe49f2e963a88766689f8d673aa6342e
David Silva <omard.skp@gmail.com>
2018-10-07 13:12:19 -0400

    Created failing tests for Tag and Source

--- challenges/tests/test_requests/test_challenges.py
+++ challenges/tests/test_requests/test_challenges.py
@@ -1,22 +1,22 @@
 from django.test import TestCase
 from rest_framework.test import APIClient, APITestCase
 from accounts.models import User
-from challenges.models import Challenge
+from challenges.models import Challenge, Tag, Attachment, Source
 import sure
 import json
+import pdb
 
 class ChallengeApiTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.content_type = 'application/vnd.api+json'
 
-    def test_challenge_create(self):
-        """
-        Ensure we get the correct challenge
-        """
-        user = User.objects.filter(username="codecorgi").first()
+    def get_sample_challenge(self, user=None, tags=None):
+        if user is None:
+            user = User.objects.filter(username="codecorgi").first()
 
-        challenge = Challenge.objects.create(user=user,
+        challenge = Challenge.objects.create(
+            user=user,
             title = 'Test Challenge',
             short_title = 'Test Challenge',
             owner = 'Gandalf',
@@ -31,6 +31,18 @@ class ChallengeApiTests(APITestCase):
             code_tips = 'Use a linter',
         )
 
+        if not tags is None:
+            challenge.tags.set(tags)
+
+        return challenge
+
+
+    def test_challenge_create(self):
+        """
+        Ensure we get the correct challenge
+        """
+        challenge = self.get_sample_challenge()
+
         response = self.client.get(f'/services/api/challenges?pk={ challenge.id }',
                                    content_type=self.content_type)
 
@@ -38,5 +50,46 @@ class ChallengeApiTests(APITestCase):
 
         response_data = json.loads(response.content)
         attributes = response_data['data'][0]['attributes']
-        relationships = response_data['data'][0]['relationships']
         attributes['title'].should.equal(challenge.title)
+
+    def test_challenge_create_with_tags(self):
+        """
+        Ensure we get the correct challenge
+        """
+        tag1 = Tag.objects.create(name='Javascript')
+
+        challenge = self.get_sample_challenge(tags=[ tag1 ])
+
+        response = self.client.get(f'/services/api/challenges?pk={ challenge.id }',
+                                   content_type=self.content_type)
+
+        response.status_code.should.equal(200)
+
+        response_data = json.loads(response.content)
+        attributes = response_data['data'][0]['attributes']
+        included = response_data['included']
+
+        attributes['title'].should.equal(challenge.title)
+
+        [item for item in included if item.get('type') == 'tags'][0]['attributes']['name'].should.equal(tag1.name)
+
+    def test_challenge_create_with_source(self):
+        """
+        Ensure we get the correct challenge
+        """
+        challenge = self.get_sample_challenge()
+        source = Source.objects.create(
+            challenge=challenge,
+            name='github',
+            url='https://github.com/corgicode'
+        )
+
+        response = self.client.get(f'/services/api/challenges?pk={ challenge.id }',
+                                   content_type=self.content_type)
+
+        response.status_code.should.equal(200)
+
+        response_data = json.loads(response.content)
+        included = response_data['included']
+
+        [item for item in included if item.get('type') == 'sources'][0]['attributes']['name'].should.equal(source.name)
